f3c9332ce94c8923b43c91ff8ed3663e9aea791c
neonium <ericdvachon@gmail.com>
2014-12-17 21:57:04 -0700

    post.php changes
    There are now dates and a report button (report-post not implemented
    yet). Post ownership is still bugged and needs fixed.

--- Web-app/posts.php
+++ Web-app/posts.php
@@ -3,10 +3,6 @@
 // First we execute our common code to connection to the database and start the session
 require ("common.php");
 
-// Store previoue page location incase of new post
-if(!isset($_GET['views']))
-	$_GET['views'] = 0;
-
 $_SESSION ['previous_page'] = $_SERVER ['PHP_SELF'] . "?id=" . $_GET ['id'] . "&obj=" . $_GET ['obj'] . "&views=" . $_GET ['views'] . "&posts=" . $_GET['posts'];
 
 if (! empty ( $_GET )) {
@@ -74,43 +70,37 @@ include ("templates/header.php");
 		<div class="margin">
 			<!-- CONTENT -->
 			<section class="s-12 l-9 right">
-					<?php foreach ($rows as $row) {?>
+					<?php foreach ($rows as $row) :?>
 					<table class="responstable">
-					<tr>
-						<th>User: <?php echo($row['Username']);?> </th>
-					</tr>
-
-					<tr>
-						<td><?php echo($row['Body'] ."\t"); ?> </td>
-					</tr>
-
-					<tr>
-						<td><?php if($row['User_id'] == $_SESSION ['user']['User_id'] and $thread_status !== '1') { ?>
-							<a
-							href="edit-post.php?p_id=<?php echo($row['Post_id']);?>&t_id=<?php echo($row['Thread_id']);?>">Edit</a>
-						</td>
-							<?php 
-							} ?>
-					</tr>
-				</table>
-				<br />
-				<br /> 
-				
-					 
-					<?php }?>
-					
+						<tr> <th>Posted by <?php echo($row['Username']);?> </th> </tr>
+						<tr> <td><?php echo($row['Body'] ."\t"); ?> </td> </tr>
+						<tr>
+							<td>
+								<a style="font-size: 75%"> Posted on: <?php echo($row['Date']);?> </a>
+								<?php if($row['User_id'] == $_SESSION ['user']['User_id'] and $thread_status !== '1') : ?>
+									<a	href="edit-post.php?p_id=<?php echo($row['Post_id']);?>&t_id=<?php echo($row['Thread_id']);?>" style="font-size: 75%; float:right" >Edit</a>
+								<?php elseif (!$row['User_id'] <> $_SESSION ['user']['User_id'] and $thread_status !== '1') :?>
+									<a	href="report-post.php?p_id=<?php echo($row['Post_id']);?>&t_id=<?php echo($row['Thread_id']);?>" style="font-size: 75%; float:right" >Report</a>
+								<?php endif?>
+							</td>
+						</tr>
+					</table>
+					<?php endforeach;?>
+					<br />
+					<br /> 
 					<!-- Only show option to post if thread is not locked. Else show locked text.-->
 					<?php if ($thread_status !== '1') :?>
-					
+				
 					<!--Add new post-->
-					<form action="newpost.php?posts=<?php echo($_GET['posts']);?>&id=<?php echo($_GET['id']);?>&views=<?php echo($_GET['views']);?>" method="post" class="customform s-12 l-8">
-               		<p>New Post:</p>
-               		<textarea name="text_post"style="width: 808px; height: 121px;"></textarea>
-               		<input type="hidden" name="thread_id" value="<?php echo($_GET['id']);?>">
-               		<input type="hidden" name="obj" value="<?php echo($_GET['obj']);?>">
-               		<div class="s-9" ><button type="submit">Post</button></div>
-             		</form>
+					<form action="newpost.php?posts=<?php echo($_GET['posts']);?>&id=<?php echo($_GET['id']);?>&views=<?php echo($_GET['views']);?>" method="post" class="customform s-12 l-12">
+           				<p>New Post:</p>
+           				<textarea name="text_post"style=" height: 121px;"></textarea>
+           				<input type="hidden" name="thread_id" value="<?php echo($_GET['id']);?>">
+           				<input type="hidden" name="obj" value="<?php echo($_GET['obj']);?>">
+           				<div class="s-9 l-2" ><button type="submit">Post</button></div>
+           			</form>
 					
+					<!-- Show thread locked -->
 					<?php else :?>
 					<form method="post" class="customform s-12">
 						<button class="customform s-2" type="submit">Thread Locked</button>
@@ -122,7 +112,4 @@ include ("templates/header.php");
 </div>
 
 <!-- FOOTER -->
-<?php
-
-include ("templates/footer.php");
-?>
+<?php include ("templates/footer.php"); ?>
