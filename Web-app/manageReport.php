a7371da3b1224b8436db268f52de4896af4e2e8f
neonium <ericdvachon@gmail.com>
2014-12-23 19:31:46 -0700

    Admin tools report response implemented

--- Web-app/manageReport.php
+++ Web-app/manageReport.php
@@ -29,13 +29,57 @@ if(!empty($_GET)){
 	
 	$view = new viewServer();
 	
-	if(isset($report['Post_id'])){
-		// Query to select threads and topics
+
+	// Query to select all posts in the thread of the reported post
+	$query2 = "	SELECT	*,
+			 			p.User_id as User_id,
+			 			p.Object_id as Object_id
+				FROM	Post as p JOIN User as u on p.User_id = u.User_id JOIN Thread as t on t.Thread_id = p.Thread_id
+				WHERE	t.Thread_id = :thread_id";
+	
+	$query_params2 = array (
+			':thread_id' => $report['Thread_id'],
+	);
+	
+	try {
+		// Execute the query against the database
+		$stmt = $db->prepare ($query2);
+		$stmt->execute ($query_params2);
+	
+		$rows = $stmt->fetchAll ();
+	} catch ( PDOException $ex ) {
+		die ( "Failed to run query: " . $ex->getMessage () );
+	}
+	
+	// Query to select all posts in this thread
+	$query3 = "	SELECT	*
+				FROM	Thread
+				WHERE	Thread_id = :thread_id";
+	
+	$query_params3 = array (
+			':thread_id' => $report['Thread_id'],
+	);
+	
+	try {
+		// Execute the query against the database
+		$stmt = $db->prepare ( $query3 );
+		$result = $stmt->execute ( $query_params3 );
+			
+		$thread = $stmt->fetch ();
+	} catch ( PDOException $ex ) {
+		die ( "Failed to run query: " . $ex->getMessage () );
+	}
+	
+	$view->thread = $thread;
+	$view->rows = $rows;
+	
+	if($report['Post_id'] != 0){
+		// Query to the reported post
 		$query = "	SELECT	*
-					FROM	Report JOIN Post as p on Object_id = p.Object_id
+					FROM	Report as r JOIN Post as p on r.Object_id = p.Object_id
 					WHERE	Report_id = :report_id";
 		$query_params = array (	':report_id' => $_GET['r_id']);
-			
+		
 		try {
 			// Execute the query against the database
 			$stmt = $db->prepare ($query);
@@ -45,14 +89,14 @@ if(!empty($_GET)){
 		} catch ( PDOException $ex ) {
 			die ( "Failed to run query: " . $ex->getMessage () );
 		}
-	
+				
 		$view->report = $report;
 			
-		$view->render("managePostReport.phtml");
+		$view->render("manageReport.phtml");
 	}else{
 		// Query to select threads and topics
 		$query = "	SELECT	*
-					FROM	Report JOIN Thread as t on Object_id = t.Object_id
+					FROM	Report as r JOIN Thread as t on r.Object_id = t.Object_id
 					WHERE	Report_id = :report_id";
 		$query_params = array (	':report_id' => $_GET['r_id']);
 		
@@ -68,7 +112,7 @@ if(!empty($_GET)){
 		
 		$view->report = $report;
 	
-		$view->render("manageThreadReport.phtml");		
+		$view->render("manageReport.phtml");		
 	}
 }
 ?>
